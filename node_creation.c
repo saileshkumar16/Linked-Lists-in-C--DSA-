998a16164cd9fa0f8e8f17f0b8980224ef0e9de7
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-06 19:05:52 +0530

    Add files via upload

---
+++ node_creation.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+int main()
+{
+    struct Node* first = (struct Node *)malloc(sizeof(struct Node));
+    struct Node* second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node* third = (struct Node *)malloc(sizeof(struct Node));
+    
+    first -> data = 10;
+    first -> next = second;
+    
+    second->data = 20;
+    second->next = third;
+    
+    third->data = 30;
+    third->next = NULL;
+    
+    printf("%d %d %d %p %p %p",first->data, second->data, third->data, first->next, second->next, third->next);
+
+    
+
+    return 0;
+}
