6a1541fe1aa98759f9a78f6c065bce97deea52d7
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-08-17 23:28:23 +0530

    Update node_creation.c

--- node_creation.c
+++ node_creation.c
@@ -28,3 +28,42 @@ int main()
 
     return 0;
 }
+
+-----------------------------------------------------------------------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+    struct node
+    {
+        int data;
+        struct node *next;
+    };
+
+int main() 
+{
+    struct node *head,*middle,*last;
+    
+    head = malloc(sizeof(struct node));
+    middle = malloc(sizeof(struct node));
+    last = malloc(sizeof(struct node));
+    
+    head-> data = 10;
+    middle ->data = 20;
+    last -> data = 30;
+    
+    head->next = middle;
+    middle ->next = last;
+    last ->next = NULL;
+    
+    struct node *temp = head;
+    
+    while(temp!=NULL)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+
+
+    return 0;
+}
+
