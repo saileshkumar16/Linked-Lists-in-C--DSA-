4c1957997d6b3c86411d00c0d396eedcef3c17bc
Sailesh Kumar Pasam <144588637+saileshkumar16@users.noreply.github.com>
2025-11-29 14:42:20 +0530

    Update node_insertion_beginning.c

--- node_insertion_beginning.c
+++ node_insertion_beginning.c
@@ -1,45 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-    struct node
-    {
-        int data;
-        struct node *next;
-    };
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+// Insert ONE node at beginning
+struct node* insert_at_beginning(struct node *head, int value)
+{
+    struct node *newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = value;
+    newNode->next = head;   // link new node to old head
+    return newNode;         // new head
+}
 
 int main() 
 {
-    struct node *head = NULL; //empty list 
-    
-    struct node *newnode1 = malloc(sizeof(struct node)); //create new node
-    
-    newnode1->data = 10; //give value
-    newnode1->next = head; // point the next address
-    
-    head = newnode1; // update the head
-    
-    struct node *newnode2 = malloc(sizeof(struct node));
-    
-    newnode2->data = 20;
-    newnode2->next = head;
-    
-    head = newnode2;
-    
-    struct node *newnode3 = malloc(sizeof(struct node));
-    
-    newnode3->data = 30;
-    newnode3->next = head;
-    
-    head = newnode3;
-    
-    struct node *temp = head;
-    
-    
-    while(temp!=NULL)
-    {
-        printf("%d ",temp->data);
+    struct node *first, *second, *third;
+    struct node *head, *temp;
+
+    // Allocate 3 nodes
+    first  = (struct node*)malloc(sizeof(struct node));
+    second = (struct node*)malloc(sizeof(struct node));
+    third  = (struct node*)malloc(sizeof(struct node));
+
+    // Assign data
+    first->data  = 10;
+    second->data = 20;
+    third->data  = 30;
+
+    // Link nodes
+    first->next  = second;
+    second->next = third;
+    third->next  = NULL;
+
+    // Set head
+    head = first;
+
+    // Insert new node at beginning
+    head = insert_at_beginning(head, 5);
+
+    // Print linked list + count
+    int count = 0;
+    temp = head;
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
         temp = temp->next;
+        count++;
     }
 
+    printf("NULL\n");
+    printf("Node count = %d\n", count);
 
     return 0;
 }
